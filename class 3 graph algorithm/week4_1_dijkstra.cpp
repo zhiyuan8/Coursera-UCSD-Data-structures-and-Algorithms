a13899d194974f10c70c04f0f232dccad1d9d7de
Zhiyuan Jim Li <39573601+zhiyuan8@users.noreply.github.com>
2020-03-09 00:19:36 -0700

    Add files via upload

--- class 3 graph algorithm/week4_1_dijkstra.cpp
+++ class 3 graph algorithm/week4_1_dijkstra.cpp
@@ -1,15 +1,90 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <limits>
 
-using std::vector;
-using std::queue;
-using std::pair;
-using std::priority_queue;
+using namespace std;
+#define INT_MAX numeric_limits<int>::max()
+
+void print_dist(vector<int> &dist){
+  cout << "print_dist";
+  for (auto x:dist) {
+    cout << x << ' ';
+  }
+  cout << endl;
+}
+
+void print_vector_of_vector(vector<vector<int> > &vec){
+  for (size_t i=0; i< vec.size(); i++) {
+    cout << "node: " << i << " ";
+    for (auto x:vec[i]) {
+      cout << x << " ";
+    }
+    cout << endl;
+  }
+}
+
+int pick_vertex(vector<vector<int> > &adj, vector<int> &dist, vector<bool> &sptSet){
+  // Pick the vertex with minimum distance value and not already included in SPT
+  // for min-Heap, it takes O(logV), but vector takes O(V)
+  int min_val = INT_MAX;
+  int min_index = 0;
+  for (int i=0; i<adj.size(); i++){
+      if (dist[i] <= min_val && sptSet[i] == false){ // dist[i] <= min_val, otherwise isolated node will never be selected
+        min_val = dist[i];
+        min_index = i;
+        //cout << "In pick_vertex, min_index= " << min_index << endl;
+      }
+  }
+  return min_index;
+}
 
 int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-  //write your code her
-  return -1;
+  // refer from: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+  // adj: adjacent list representation
+  // dist[V]: save the dist from node s to all nodes
+  // sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
+
+  // step 1: initialization
+  int num_V = adj.size();
+  vector<int> dist(num_V,INT_MAX);
+  vector<bool> sptSet(num_V,false);
+
+  // step 2: set up src node s
+  dist[s] = 0;
+
+  // Find shortest path for all vertices
+  for (int i = 0; i < num_V; i++){ // loop over all vertices
+
+    // step 3: Pick the vertex with minimum distance value and not already included in SPT, and mark as explored
+    int u = pick_vertex(adj, dist, sptSet);
+    //cout << "loop: " << i << " pick vertex: " <<u << endl;
+    //print_dist(dist); // to debug
+    sptSet[u] = true;
+
+    // step 4: Update dist value of the adjacent vertices of the picked vertex.
+    for (int j = 0; j < adj[u].size(); j++){ // cannot iterate adj[u], because cost[][] has same index as adj[][]
+      // Update dist[v] if <1> v is not in sptSet <2> dist[v] > dist[u] + w[u][v]
+      int v = adj[u][j]; // get the neighbor of node u
+      //cout << "u " << u << " v " << v << " sptSet[v] " << sptSet[v] << " dist[u] " << dist[u] << " dist[v] " << dist[v] << " cost[u][v] " << cost[u][j] << endl;
+      if (sptSet[v] == false && (dist[u] != INT_MAX) ){ // avoid "dist[u] + cost[u][v]" go above INT_MAX
+        if (dist[v] > (dist[u] + cost[u][j])) {
+              dist[v] = dist[u] + cost[u][j];
+        }
+      }
+    }
+    //print_dist(dist); // to debug
+
+    if (sptSet[t]){ // node t is explored, break for loop
+      break;
+    }
+  }
+
+  if (dist[t] == INT_MAX){
+      return -1; // cannot found, return -1
+  } else {
+      return dist[t];
+  }
 }
 
 int main() {
@@ -23,6 +98,10 @@ int main() {
     adj[x - 1].push_back(y - 1);
     cost[x - 1].push_back(w);
   }
+  //cout << "print out adj " << endl;
+  //print_vector_of_vector(adj);
+  //cout << "print out cost " << endl;
+  //print_vector_of_vector(cost);
   int s, t;
   std::cin >> s >> t;
   s--, t--;
--- class 3 graph algorithm/week4_2_negative_cycle.cpp
+++ class 3 graph algorithm/week4_2_negative_cycle.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <limits>
+// cannot pass case 5...
+using namespace std;
+#define INTMAX numeric_limits<int>::max()
 
-using std::vector;
+vector<int> BellmanFord(vector<vector<int> > &adj, vector<vector<int> > &cost){
+  int src = 0; // source node is 0
 
-int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
+  // step 1: initialization
+  vector<int> dist (adj.size(),INTMAX);
+  dist[src] = 0;
+
+  // step 2: update dist
+  for (size_t i = 0; i < adj.size(); i++) {
+    for (size_t j = 0; j < adj[i].size(); j++) {
+      int u = i;
+      int v = adj[i][j];
+      int w = cost[i][j];
+      if (dist[u] != INTMAX){ // avoid int overflow
+        if (dist[u] + w < dist[v]){
+          dist[v] = dist[u] + w;
+        }
+      }
+    }
+  }
+  return dist;
+}
+
+bool negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
   //write your code here
-  return 0;
+  vector<int> dist = BellmanFord(adj , cost);
+
+  // Step 3: check for negative-weight cycles.
+  // If we get a shorter path, then there is a cycle.
+  for (size_t i = 0; i < adj.size(); i++) {
+    for (size_t j = 0; j < adj[i].size(); j++) {
+      if (dist[i] != INTMAX){ // avoid int overflow
+        if (dist[i] + cost[i][j] < dist[adj[i][j]]){
+          return true; // find negative cycle
+        }
+      }
+    }
+  }
+  return false;
 }
 
 int main() {
