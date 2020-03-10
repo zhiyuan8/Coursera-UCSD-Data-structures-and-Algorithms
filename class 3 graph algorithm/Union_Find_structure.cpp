78222ed837152cb95dc49c45effb896a3da1c16d
Zhiyuan Jim Li <39573601+zhiyuan8@users.noreply.github.com>
2020-03-09 19:32:22 -0700

    Add files via upload

---
+++ class 3 graph algorithm/Union_Find_structure.cpp
@@ -0,0 +1,48 @@
+// refer from https://www.geeksforgeeks.org/union-find/
+// disjoint set: https://www.tutorialspoint.com/cplusplus-program-to-implement-disjoint-set-data-structure
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class DisjointSet { //to represent disjoint set
+   unordered_map<int, int> parent; // represent of node i
+   public:
+   void makeSet(vector<int> const &wholeset){
+      for (int i : wholeset) // create n disjoint sets
+      parent[i] = i;
+   }
+   int Find(int l) { // Find the root of the set in which element l belongs
+      if (parent[l] == l) // if l is root
+         return l;
+      return Find(parent[l]); // recurs for parent till we find root
+   }
+   void Union(int m, int n) { // perform Union of two subsets m and n
+      int x = Find(m);
+      int y = Find(n);
+      parent[x] = y;
+   }
+};
+
+void print(vector<int> const &vec, DisjointSet &disjoint_set) {
+   for (int i : vec)
+   cout << disjoint_set.Find(i) << " ";
+   cout << '\n';
+}
+
+int main() {
+   vector<int> wholeset = { 6,7,1,2,3 }; // items of whole set
+   DisjointSet dis; //initialize DisjointSet class
+   dis.makeSet(wholeset); // create individual set of the items of wholeset
+   dis.Union(7, 6); // 7,6 are in same set
+   print(wholeset, dis);
+   if (dis.Find(7) == dis.Find(6)) // if they are belong to same set or not.
+      cout<<"Yes"<<endl;
+   else
+      cout<<"No";
+   if (dis.Find(3) == dis.Find(4))
+      cout<<"Yes"<<endl;
+   else
+      cout<<"No";
+   return 0;
+}
--- class 3 graph algorithm/week5_1_connecting_points.cpp
+++ class 3 graph algorithm/week5_1_connecting_points.cpp
@@ -1,23 +1,82 @@
-#include <algorithm>
+#include <algorithm> // sort()
 #include <iostream>
-#include <iomanip>
+#include <unordered_map> // for DisjointSet
+#include <iomanip> // setprecision()
 #include <vector>
-#include <cmath>
+#include <cmath> // sqrt()
+#include <utility>  // for pair()
+// use: vector<pair<double, pair<int, int> > > edges, then weight can be "index"
+using namespace std;
 
-using std::vector;
+class DisjointSet { //to represent disjoint set
+   unordered_map<int, int> parent; // represent of node i
+   public:
+   DisjointSet(int V) { // Default Constructor
+      for (int i=0;i<V;i++){ // create n disjoint sets
+      parent[i] = i;
+      }
+   }
+   int Find(int l) { // Find the root of the set in which element l belongs
+      if (parent[l] == l) // if l is root
+         return l;
+      return Find(parent[l]); // recurs for parent till we find root
+   }
+   void Union(int m, int n) { // perform Union of two subsets m and n
+      int x = Find(m);
+      int y = Find(n);
+      parent[x] = y;
+   }
+};
 
-double minimum_distance(vector<int> x, vector<int> y) {
-  double result = 0.;
+void print_edges(vector<pair<double, pair<int, int> > > &edges){
+  for (auto edge:edges) {
+    double w = edge.first;
+    int u = edge.second.first;
+    int v = edge.second.second;
+    cout << "u: " << u << " v: " << v << " w: " << w << endl;
+  }
+}
+
+double minimum_distance(vector<pair<double, pair<int, int> > > &edges, size_t V) {
+  double result = 0.; // the total weight of mst
   //write your code here
+  // step 1: sort edges according to weight
+  sort(edges.begin(), edges.end()); // will sort by first of pair
+  //cout << "after sorting:" << endl;
+  //print_edges(edges);
+  // step 2: create DisjointSet
+  DisjointSet distjoint(V);
+  // step 3: loop from smallest weighted edge
+  for (auto edge:edges) {
+      int u = edge.second.first;
+      int v = edge.second.second;
+      //cout << "u " << u << " " << distjoint.Find(u) << " v " << v << " " << distjoint.Find(v)<<endl;
+      if (distjoint.Find(u) != distjoint.Find(v)){ // not in same group, no cycle
+        //cout << "find " << u << ' ' << v << endl;
+        result += edge.first;
+        distjoint.Union(u,v); // Union u & v
+      }
+  }
   return result;
-} 
+}
 
 int main() {
   size_t n;
   std::cin >> n;
   vector<int> x(n), y(n);
+  vector<pair<double, pair<int, int> > > edges;
+
   for (size_t i = 0; i < n; i++) {
-    std::cin >> x[i] >> y[i];
+    std::cin >> x[i] >> y[i]; // get the x y position of each point
   }
-  std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+  for (size_t i = 0; i < n; i++) { // get weight of each edge
+    for (size_t j = 0; j < n; j++) {
+      if (i!=j){
+        edges.push_back( { sqrt( pow(x[i]-x[j],2) + pow(y[i]-y[j],2) ) ,
+           { i , j } } );
+      }
+    }
+  }
+  //print_edges(edges);
+  std::cout << std::setprecision(10) << minimum_distance(edges, n) << std::endl;
 }
